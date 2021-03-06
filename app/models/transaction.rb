1cc592207e45ff07180325333e6526b5986b9372
sanguvk123 <sanguvk123@gmail.com>
2021-03-06 11:24:40 +0530

    transactions added to users

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,13 @@
-class Transaction < ApplicationRecord 
-  belongs_to :receiver, foreign_key: :receiver_id, class_name: "User"  
+class Transaction < ApplicationRecord
+  belongs_to :receiver, foreign_key: :receiver_id, class_name: "User"
   belongs_to :sender, foreign_key: :sender_id, class_name: "User"
-  def create_transaction(sender_acc_no, receiver_acc_no)  
-    sender_acc = Account.find_by(account_number: sender_acc_no) 
-    receiver_acc = Account.find_by(account_number: receiver_acc_no) 
-    sender_acc.balance -= transaction_amount 
-    receiver_acc.balance += transaction_amount 
-    sender_acc.save 
-    receiver_acc.save 
-  end 
+  def create_transaction(sender_acc_no, receiver_acc_no)
+    sender_acc = Account.find_by(account_no: sender_acc_no)
+    receiver_acc = Account.find_by(account_no: receiver_acc_no)
+    sender_acc.balance -= transaction_amount
+
+    receiver_acc.balance += transaction_amount
+    sender_acc.save
+    receiver_acc.save
+  end
 end
